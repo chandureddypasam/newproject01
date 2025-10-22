04080f0b442f2a3a349be3357c0a5442a74dbe5c
Chandrasekhharreddy <144087291+Chandrasekhharreddy@users.noreply.github.com>
2025-10-23 00:34:36 +0530

    Added heading

--- newpro/src/App.js
+++ newpro/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Today We will be learning about the remote repository</h1>
+      <h1>and also learnt about how to pull and push the modified code</h1>
     </div>
   );
 }
